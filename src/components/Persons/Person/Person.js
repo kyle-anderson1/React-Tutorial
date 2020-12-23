142da092382142528c54407df75f44791408d558
kyle-anderson1 <kanderson12896@gmail.com>
2020-12-23 13:04:30 -0800

    3rd: Minor updates

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,7 +5,7 @@ const person = (props) => {
 
   return (
     <div className={classes.Person}>
-      <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old!</p>
+      <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name}/>
     </div>
